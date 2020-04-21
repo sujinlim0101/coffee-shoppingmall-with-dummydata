6a29976c03ae95102c31705b0400f9bc38866acd
sujin <mimitn@hanmail.net>
2020-04-21 19:07:50 +0900

    메인 텍스트 크기 조정

--- src/components/HomeMain2.js
+++ src/components/HomeMain2.js
@@ -9,13 +9,13 @@ class HomeMain2 extends React.Component{
             <div>
                 <div className="container">
                     <div className="" style={{marginTop:"50px"}}>
-                        <div className="homeText px-5"><h4 className="pb-3 px-5 mb-3">오늘 이런 커피 어때요?</h4></div>
+                        <div className="homeText px-5"><h3 className="pb-3 px-5 mb-3 pt-3">커피 한잔 어때요?</h3></div>
                      <ItemList item={"featuredBeans"} ></ItemList>
                     </div>
                 </div>
                 <div className="mt-3" style={{backgroundColor:"#f5f4f1"}}>
                     <div className="container">
-                        <div className="homeText px-5 pt-5"><h4 className="pb-3 px-5 mb-3">요즘 뜨는 커피 도구</h4></div>
+                        <div className="homeText px-5 pt-5"><h3 className="pb-3 px-5 mb-3 pt-3">요즘 뜨는 커피 도구</h3></div>
                         <ItemList item={"featuredTools"} ></ItemList>
                     </div>
                 </div>
