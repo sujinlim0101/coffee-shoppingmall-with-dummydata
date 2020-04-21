b05b3c8988db5a529ce22f51b1bfefdfb4b440b2
sujin <mimitn@hanmail.net>
2020-04-21 18:58:42 +0900

    main carosel 등 css 수정

--- src/components/HomeMain2.js
+++ src/components/HomeMain2.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ItemList from "./ItemList";
+import "./css/HomeSection2.css";
 
 
 class HomeMain2 extends React.Component{
@@ -8,16 +9,13 @@ class HomeMain2 extends React.Component{
             <div>
                 <div className="container">
                     <div className="" style={{marginTop:"50px"}}>
-                        <div className="px-5 container"><h4 className="pb-3 px-5 mb-3" 
-                            style={{textAlign:"center", borderBottom:"1.2px solid rgb(219, 200, 178)" ,position:"relative", lineHeight:"1.2"}}>
-                                오늘 이런 커피 어때요?</h4></div>
+                        <div className="homeText px-5"><h4 className="pb-3 px-5 mb-3">오늘 이런 커피 어때요?</h4></div>
                      <ItemList item={"featuredBeans"} ></ItemList>
                     </div>
                 </div>
                 <div className="mt-3" style={{backgroundColor:"#f5f4f1"}}>
                     <div className="container">
-                        <div className="px-5 pt-5"><h4 className="pb-3 px-5 mb-3" 
-                            style={{textAlign:"center", borderBottom:"1.2px solid rgb(241, 193, 139)" ,position:"relative", lineHeight:"1.2"}}>요즘 뜨는 커피 도구</h4></div>
+                        <div className="homeText px-5 pt-5"><h4 className="pb-3 px-5 mb-3">요즘 뜨는 커피 도구</h4></div>
                         <ItemList item={"featuredTools"} ></ItemList>
                     </div>
                 </div>
--- src/components/MyNav.js
+++ src/components/MyNav.js
@@ -16,14 +16,14 @@ class MyNav extends React.Component {
                              width={140} height={50}/>
                     </Link>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
-                    <Navbar.Collapse id="responsive-navbar-nav" >
+                    <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="" >
-                            <Link to="/beans" className={"nav2 nav1 mx-5 "} style={{fontWeight:"400", textAlign:"center" ,color:"#433f3f"}}>BEANS</Link>
-                            <Link to="/tools" className={"nav2 nav1 mx-5"} style={{fontWeight:"400", textAlign:"center" ,color:"#433f3f"}}>TOOLS</Link>
-                            <Link to="/about" className={"nav2 nav1 mx-5"} style={{fontWeight:"400", textAlign:"center" ,color:"#433f3f"}}>ABOUT US</Link>
-                            <Link to="/contact" className={"nav2 nav1 mx-5"} style={{fontWeight:"400", textAlign:"center" ,color:"#433f3f"}}>CONTACT US</Link>
-                            <Link to="/login" className={"nav2 nav1 mr-2 mt-2"} style={{fontSize:"0.7em" ,textAlign:"center", float: "right"}}>로그인</Link>
-                            <Link to="/signup" className={"nav2 nav1 mr-2 mt-2"}  style={{fontSize:"0.7em" ,textAlign:"center", float: "right"}}>회원가입</Link>
+                            <Link to="/beans" className={"nav2 nav1 mx-5 category"}>BEANS</Link>
+                            <Link to="/tools" className={"nav2 nav1 mx-5 category"}>TOOLS</Link>
+                            <Link to="/about" className={"nav2 nav1 mx-5 category"}>ABOUT US</Link>
+                            <Link to="/contact" className={"nav2 nav1 mx-5 category"}>CONTACT US</Link>
+                            <Link to="/login" className={"nav2 nav1 mr-2 mt-2 linkLogin"}>로그인</Link>
+                            <Link to="/signup" className={"nav2 nav1 mr-2 mt-2 linkLogin"}  style={{fontSize:"0.7em" ,textAlign:"center", float: "right"}}>회원가입</Link>
                           
                         </Nav>
                         <Nav>
