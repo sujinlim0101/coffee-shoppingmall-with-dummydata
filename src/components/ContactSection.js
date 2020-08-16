acb4f9046fdb0188e66791520a3eed69689e3edc
sujinlim0101 <60640614+sujinlim0101@users.noreply.github.com>
2020-08-16 22:59:54 +0900

    Add comments and Set Height in toast-message on Detail.js

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -1,39 +1,75 @@
 import React from "react";
 
-class ContactSection extends React.Component{
-    render(){
-        window.scrollTo(0, 0);
-        return (
-            <div className="container mb-5" style={{textAlign:"center"}}>
-                <h1 className="mt-5" style={{color:"#7d5341"}}>Contact <span style={{color:"#ff8550"}}>us!</span></h1>
-                <div>
-                    
-                    <h4 style={{color:"#7d5341"}}>[ <span  style={{color:"#ff8550"}}> DayLight Coffee Roasters Wholesale </span> ]</h4>
-                    <br/>
-                    
-                    <img className="img-fluid"src={require('./../images/dalgona.jpg')} style={{marginLeft:'auto', marginRight:"auto", display:"block"}} alt={'dalgona'}/>
-                    <h3 style={{ textDecoration: "underline"}} className="mt-4 mb-4">Special Coffee</h3>
-                        <li>좋은 질의 커피</li>
-                        <li>꼼꼼한 커피 선별 작업</li>
-                        <li>48시간 안에 로스팅</li>
-                </div>
-                
-                <h3 className="mt-4 mb-4" style={{ textDecoration: "underline"}}>Wholesale</h3>
-              
-                <div>
-                    <p>혹시, 맛있고, 신선한 커피를 원하신다면?</p>
-                    <p>카페에 원두를 원하신다면?</p>
-                    <p>소량 판매 뿐만 아니라, 많은 업주님에게 도매로도 판매합니다.</p>
-                    <p>주말을 제외한 9시부터 6시까지 상담을 진행합니다.</p>
-                    <p>꼭 결정한 후에 상담할 필요는 없어요.</p>
-                    <p>편한 마음으로 저희에게 연락주세요!</p>
-                    <p className="mb-5">소매/도매커피에 대해 상담을 진행하고 있습니다! </p>
-                    Contact : <a href="mailto:sujinlim716800@gmail.com" style={{color:"#ff6600", fontWeight:"bold"}}>데이라이트<button className="btn btn-outline-secondary mx-2" type="button" style={{fontSize:"small"}}>Mail</button></a><br/> 
-              
-                </div>
-            </div>
-        );
-    }
+class ContactSection extends React.Component {
+  render() {
+    window.scrollTo(0, 0);
+    return (
+      <div className="container mb-5" style={{ textAlign: "center" }}>
+        <h1 className="mt-5" style={{ color: "#7d5341" }}>
+          Contact <span style={{ color: "#ff8550" }}>us!</span>
+        </h1>
+        <div>
+          <h4 style={{ color: "#7d5341" }}>
+            [{" "}
+            <span style={{ color: "#ff8550" }}>
+              {" "}
+              DayLight Coffee Roasters Wholesale{" "}
+            </span>{" "}
+            ]
+          </h4>
+          <br />
+
+          <img
+            className="img-fluid"
+            src={require("./../images/dalgona.jpg")}
+            style={{
+              marginLeft: "auto",
+              marginRight: "auto",
+              display: "block",
+            }}
+            alt={"dalgona"}
+          />
+          <h3 style={{ textDecoration: "underline" }} className="mt-4 mb-4">
+            Special Coffee
+          </h3>
+          <li>좋은 질의 커피</li>
+          <li>꼼꼼한 커피 선별 작업</li>
+          <li>48시간 안에 로스팅</li>
+        </div>
+
+        <h3 className="mt-4 mb-4" style={{ textDecoration: "underline" }}>
+          Wholesale
+        </h3>
+
+        <div>
+          <p>혹시, 맛있고, 신선한 커피를 원하신다면?</p>
+          <p>카페에 원두를 원하신다면?</p>
+          <p>소량 판매 뿐만 아니라, 많은 업주님에게 도매로도 판매합니다.</p>
+          <p>주말을 제외한 9시부터 6시까지 상담을 진행합니다.</p>
+          <p>꼭 결정한 후에 상담할 필요는 없어요.</p>
+          <p>편한 마음으로 저희에게 연락주세요!</p>
+          <p className="mb-5">
+            소매/도매커피에 대해 상담을 진행하고 있습니다!{" "}
+          </p>
+          Contact :{" "}
+          <a
+            href="mailto:sujin716800@gmail.com"
+            style={{ color: "#ff6600", fontWeight: "bold" }}
+          >
+            데이라이트
+            <button
+              className="btn btn-outline-secondary mx-2"
+              type="button"
+              style={{ fontSize: "small" }}
+            >
+              Mail
+            </button>
+          </a>
+          <br />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default ContactSection;
--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,97 +1,109 @@
-import React,{useState} from 'react';
-import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
-import Toast from 'react-bootstrap/Toast'
-
+import React, { useState } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Toast from "react-bootstrap/Toast";
+import "./css/Detail.css";
 class Detail extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      id : localStorage.getItem("login_email"),
+      id: localStorage.getItem("login_email"),
       isLoaded: true,
       product: {},
-      itemInfo:{
-        ea:1,
-        productID:1,
+      itemInfo: {
+        ea: 1,
+        productID: 1,
       },
-      isLoggedIn:false,
-      show:false,
-      message:""
+      isLoggedIn: false,
+      //toast가 나올지 안나올지 정하는 변수
+      show: false,
+      //성공 실패에 따른 다른 메세지 설정을 위한 변수
+      message: "",
     };
-  };
-  
+  }
+
   minus = () => {
     var ea = this.state.itemInfo.ea;
+    // 수량을 1개 이상으로 제약
     if (ea > 1) {
-      ea--
-      const item = {...this.state.itemInfo, ea:ea}
-      this.setState({itemInfo:item});
+      ea--;
+      const item = { ...this.state.itemInfo, ea: ea };
+      this.setState({ itemInfo: item });
     } else {
       ea = 1;
-      const item = {...this.state.itemInfo, ea:ea}
-      this.setState({itemInfo:item});
+      const item = { ...this.state.itemInfo, ea: ea };
+      this.setState({ itemInfo: item });
     }
-  }
+  };
 
   add = () => {
     var ea = this.state.itemInfo.ea;
+    //99개 이상은 받지 말도록 제약
     if (ea < 99) {
       ea++;
-      const item = {...this.state.itemInfo, ea:ea}
-      this.setState({itemInfo:item});
+      const item = { ...this.state.itemInfo, ea: ea };
+      this.setState({ itemInfo: item });
     } else {
       ea = 99;
-      const item = {...this.state.itemInfo, ea:ea}
-      this.setState({itemInfo:item});
+      const item = { ...this.state.itemInfo, ea: ea };
+      this.setState({ itemInfo: item });
     }
-
-  }
+  };
 
   componentDidMount() {
-      window.scrollTo(0, 0);
-      let token = this.state.id;
-      if(token){
-        this.setState({
-          isLoggedIn:true
-        })
-      }else{
-        this.setState({
-          isLoggedIn:false
-        })
-      }
-  
+    //페이지가 처음 보일떄 스크롤이 가장 위쪽에 가도록 조정
+    window.scrollTo(0, 0);
+    let token = this.state.id;
+    if (token) {
+      this.setState({
+        isLoggedIn: true,
+      });
+    } else {
+      this.setState({
+        isLoggedIn: false,
+      });
+    }
+
+    //파라미터로 들어는 상품의 ID값
     const productID = this.props.match.params.productID;
-      fetch('/B0000001.json')
-      .then(res => res.json())
-      .then((result) => {
-        this.setState({
-          isLoaded: true,
-          product: result.items,
-          itemInfo: {ea:1, productID: productID}
-        });
-      },
+    //더미 데이터를 가지고 오기 위한 값 설정
+    fetch("/B0000001.json")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            product: result.items,
+            itemInfo: { ea: 1, productID: productID },
+          });
+        },
 
         (error) => {
           this.setState({
             isLoaded: true,
-            error
+            error,
           });
           console.log(error);
         }
-      )
+      );
   }
-
-  order(){
+  //주문
+  order() {
+    //서버와 연결이 안됐기 때문에 항상 실패로 설정
     this.setState({
-      show:true,
-      message:"실패했습니다. 잠시 후 다시 시도해주세요."
-    })
+      show: true,
+      message: "실패했습니다. 잠시 후 다시 시도해주세요.",
+    });
+
+    //주문하기 버튼을 눌렀을 때
+    //id를 넘겨주고 item의 정보(id, 수량)을 보냄
+
     // fetch('http://211.63.89.156:8080/daylight/detail/order/'+this.state.id,{
-    //   method: 'POST', 
+    //   method: 'POST',
     //   headers: {
     //     'Accept': 'application/json',
     //     'Content-Type': 'application/json',
     //     'Access-Control-Allow-Credentials': true,
-    //     'Access-Control-Allow-Origin': 'http://211.63.89.156:8080/', 
+    //     'Access-Control-Allow-Origin': 'http://211.63.89.156:8080/',
     //   },
     //   body: JSON.stringify(this.state.itemInfo)
     // })
@@ -110,14 +122,18 @@ class Detail extends React.Component {
     //     })
   }
 
+  //카트
   addCart = () => {
     this.setState({
-      show:true,
-      message:"실패했습니다. 잠시 후 다시 시도해주세요."
-    })
+      show: true,
+      message: "실패했습니다. 잠시 후 다시 시도해주세요.",
+    });
+
+    //카트 버튼을 눌렀을 때
+
     // console.log(this.state.itemInfo);
     // fetch('http://211.63.89.156:8080/daylight/addcart/'+this.state.id,{
-    //   method: 'POST', 
+    //   method: 'POST',
     //   headers: {
     //     'Accept': 'application/json',
     //     'Content-Type': 'application/json',
@@ -142,48 +158,90 @@ class Detail extends React.Component {
     //   )
     //   console.log(error);
     // })
-  }
+  };
+
+  //toast에서 x버튼 눌렀을 때 닫히는 기능
   closeShow() {
     this.setState({
-      show: false
-    })
+      show: false,
+    });
   }
-  render(){ 
+
+  render() {
     return (
       <div className="container mt-5">
-        <div className="row pb-5 " style={{ borderBottom: '1px solid rgb(209, 203, 203)' }}>
+        <div
+          className="row pb-5 "
+          style={{ borderBottom: "1px solid rgb(209, 203, 203)" }}
+        >
           <div className="col-sm-5">
-            <img src={"/images/"+this.state.product.mainimg} className="img-fluid mb-2" style={{ width: '100%'}} alt={this.state.product.productID}/>
+            <img
+              src={"/images/" + this.state.product.mainimg}
+              className="img-fluid mb-2"
+              style={{ width: "100%" }}
+              alt={this.state.product.productID}
+            />
           </div>
           <div className="col-sm-7">
             <div className="mt-2">
               <h2>{this.state.product.title}</h2>
-              <span style={{ color: 'gray' }}>{this.state.product.subtitle}</span>
+              <span style={{ color: "gray" }}>
+                {this.state.product.subtitle}
+              </span>
             </div>
-            <div className="mt-5" style={{ borderBottom: '1px solid #efecec' }}>
-              <span style={{ fontSize: '0.875rem' }}>가격</span>
-              <p style={{ fontSize: '2rem' }}>{this.state.product.price}원</p>
+            <div className="mt-5" style={{ borderBottom: "1px solid #efecec" }}>
+              <span style={{ fontSize: "0.875rem" }}>가격</span>
+              <p style={{ fontSize: "2rem" }}>{this.state.product.price}원</p>
             </div>
             <div className="row">
               <div className="col-12 mt-3">
-                <table style={{ borderBottom: '1px solid #efecec', width: '103%' }}>
+                <table
+                  style={{ borderBottom: "1px solid #efecec", width: "103%" }}
+                >
                   <tbody>
-                    <tr style={{ borderBottom: '1px solid #efecec' }}>
-                      <td  className="pb-3"style={{ color: 'gray' }} width="30%">원산지</td>
+                    <tr style={{ borderBottom: "1px solid #efecec" }}>
+                      <td
+                        className="pb-3"
+                        style={{ color: "gray" }}
+                        width="30%"
+                      >
+                        원산지
+                      </td>
                       <td>{this.state.product.origin}</td>
                     </tr>
-                    <tr >
-                      <td style={{ color: 'gray' }}>수량</td>
-                      <td style={{ alignItems: 'center' }}>
+                    <tr>
+                      <td style={{ color: "gray" }}>수량</td>
+                      <td style={{ alignItems: "center" }}>
                         <form className="row mt-2 mb-2">
                           <div className="input-group col-6 col-sm-8 col-md-6 col-lg-4">
                             <div className="input-group-prepend">
-                              <button className="btn btn-outline-secondary" type="button" onClick={this.minus}
-                              >-</button>
+                              <button
+                                className="btn btn-outline-secondary"
+                                type="button"
+                                onClick={this.minus}
+                              >
+                                -
+                              </button>
                             </div>
-                            <div className="pt-2" style={{ textAlign: 'center', text:"x-small" ,width:"37px",border:"1px gray solid"}}>{this.state.itemInfo.ea}</div>
+                            <div
+                              className="pt-2"
+                              style={{
+                                textAlign: "center",
+                                text: "x-small",
+                                width: "37px",
+                                border: "1px gray solid",
+                              }}
+                            >
+                              {this.state.itemInfo.ea}
+                            </div>
                             <div className="input-group-append">
-                              <button className="btn btn-outline-secondary" type="button" onClick={this.add}>+</button>
+                              <button
+                                className="btn btn-outline-secondary"
+                                type="button"
+                                onClick={this.add}
+                              >
+                                +
+                              </button>
                             </div>
                           </div>
                         </form>
@@ -192,67 +250,132 @@ class Detail extends React.Component {
                   </tbody>
                 </table>
                 <div className="">
-                  {this.state.isLoggedIn
-                    ? 
-                        <>
-                          <Toast onClose={this.closeShow.bind(this)} show={this.state.show} delay={3000} autohide>
-                            <Toast.Header>
-                              <img
-                                src="holder.js/20x20?text=%20"
-                                className="rounded mr-2"
-                                alt=""
-                              />
-                              <strong className="mr-auto">알림</strong>
-                            </Toast.Header>
-                            <Toast.Body>{this.state.message}</Toast.Body>
-                          </Toast>
-                          <button type="button" className="btn btn-outline-success mt-3 mr-1 "
-                         style={{ width: '40%' }} onClick={this.addCart.bind(this)}>장바구니</button>       
-                        </>
-                  
-                    : <Link to="/login"><button type="button" className="btn btn-outline-success mt-3 mr-1 "
-                         style={{ width: '40%' }}>장바구니</button>
-                      </Link>}
-                  {this.state.isLoggedIn
-                    ?
-                      <button type="button" className="btn btn-success mt-3 ml-2"
-                        style={{ width: '50%' }} onClick={this.order.bind(this)}>결제</button>
-                    :
-                      <Link to="/login"><button type="button" className="btn btn-success mt-3 ml-2"
-                      style={{ width: '50%' }}>결제</button>
-                      </Link>} 
+                  {/* 로그인 여부에 따라 다른 반응(로그인 페이지 이동 or toast로 성공실패 표시)
+                  todo: 별개의 component로 분리*/}
+                  {this.state.isLoggedIn ? (
+                    <>
+                      <div className="detail-toast-container">
+                        <Toast
+                          onClose={this.closeShow.bind(this)}
+                          show={this.state.show}
+                          delay={2000}
+                          autohide
+                        >
+                          <Toast.Header>
+                            <img
+                              src="holder.js/20x20?text=%20"
+                              className="rounded mr-2"
+                              alt=""
+                            />
+                            <strong className="mr-auto">알림</strong>
+                          </Toast.Header>
+                          <Toast.Body>{this.state.message}</Toast.Body>
+                        </Toast>
+                      </div>
+                      <button
+                        type="button"
+                        className="btn btn-outline-success mt-3 mr-1 "
+                        style={{ width: "40%" }}
+                        onClick={this.addCart.bind(this)}
+                      >
+                        장바구니
+                      </button>
+                    </>
+                  ) : (
+                    <Link to="/login">
+                      <button
+                        type="button"
+                        className="btn btn-outline-success mt-3 mr-1 "
+                        style={{ width: "40%" }}
+                      >
+                        장바구니
+                      </button>
+                    </Link>
+                  )}
+                  {this.state.isLoggedIn ? (
+                    <button
+                      type="button"
+                      className="btn btn-success mt-3 ml-2"
+                      style={{ width: "50%" }}
+                      onClick={this.order.bind(this)}
+                    >
+                      결제
+                    </button>
+                  ) : (
+                    <Link to="/login">
+                      <button
+                        type="button"
+                        className="btn btn-success mt-3 ml-2"
+                        style={{ width: "50%" }}
+                      >
+                        결제
+                      </button>
+                    </Link>
+                  )}
                 </div>
               </div>
             </div>
           </div>
         </div>
         <div>
-          <img className="img-fluid mt-5 mb-4 center" src={"/images/"+this.state.product.subimg} style={{marginLeft:"auto", marginRight:"auto", display: "block"}}></img>
+          <img
+            className="img-fluid mt-5 mb-4 center"
+            src={"/images/" + this.state.product.subimg}
+            style={{
+              marginLeft: "auto",
+              marginRight: "auto",
+              display: "block",
+            }}
+          ></img>
         </div>
-        <h3 className="mt-4 pb-4 mx-5" style={{color:"#5f5c5c", textAlign:"center"}}>[{this.state.product.title}]</h3>
-        <div className="pb-4 mx-3 pt-4"style={{color:"#5f5c5c", textAlign:"center", 
-          borderTop:"1px solid rgb(250, 181, 140)", borderBottom:"1px solid rgb(250, 181, 140)",
-          backgroundColor:"#fffcf8", fontSize:"18px"}}>{this.state.product.subtitle}</div>
-        <h2  className="pb-4" style={{color:"#5f5c5c", textAlign:"center", marginTop:"80px"}}>
-          <img width={25} height={25} src={'/images/checkorange.png'}/> 데이라이트 Check Point</h2>
+        <h3
+          className="mt-4 pb-4 mx-5"
+          style={{ color: "#5f5c5c", textAlign: "center" }}
+        >
+          [{this.state.product.title}]
+        </h3>
+        <div
+          className="pb-4 mx-3 pt-4"
+          style={{
+            color: "#5f5c5c",
+            textAlign: "center",
+            borderTop: "1px solid rgb(250, 181, 140)",
+            borderBottom: "1px solid rgb(250, 181, 140)",
+            backgroundColor: "#fffcf8",
+            fontSize: "18px",
+          }}
+        >
+          {this.state.product.subtitle}
+        </div>
+        <h2
+          className="pb-4"
+          style={{ color: "#5f5c5c", textAlign: "center", marginTop: "80px" }}
+        >
+          <img width={25} height={25} src={"/images/checkorange.png"} />{" "}
+          데이라이트 Check Point
+        </h2>
         <div>
           <h4 className="mt-4">{this.state.product.descTitle1}</h4>
-          <p className="mt-3"><div dangerouslySetInnerHTML={{__html:this.state.product.descript1}} /></p>
+          <p className="mt-3">
+            <div
+              dangerouslySetInnerHTML={{ __html: this.state.product.descript1 }}
+            />
+          </p>
         </div>
         <div>
           <h4 className="mt-4">{this.state.product.descTitle2}</h4>
           <p className="mt-3">{this.state.product.descript2}</p>
         </div>
-          <div>
-            <h4 className="mt-4">{this.state.product.descTitle3}</h4>
-            <p className="mt-3">{this.state.product.descript3}</p>
-          </div>
-        
+        <div>
+          <h4 className="mt-4">{this.state.product.descTitle3}</h4>
+          <p className="mt-3">{this.state.product.descript3}</p>
+        </div>
+
         <br></br>
         <br></br>
       </div>
     );
-  };
-};
+  }
+}
 
 export default Detail;
